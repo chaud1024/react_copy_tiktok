e6c31d90f2f107e061e4303faad039bd9aa59fdd
chaud1024 <chaud1024@gmail.com>
2022-08-04 22:57:21 +0900

    commit

---
+++ react_copy_tiktok/sanity-backendcopy-tiktok/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'commet',
+    title: 'Comment',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedBy',
+            title: 'Posted By',
+            type: 'postedBy'
+        },
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string',
+        }
+    ]
+}
