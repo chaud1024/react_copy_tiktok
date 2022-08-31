891fdadd2fce0415686d213ea7b87815a05d19c1
chaud1024 <chaud1024@gmail.com>
2022-08-31 20:31:32 +0900

    add axios

--- react_copy_tiktok/pages/index.tsx
+++ react_copy_tiktok/pages/index.tsx
@@ -1,13 +1,34 @@
-import type { NextPage } from 'next'
+import { Video } from "../types";
 
-const Home: NextPage = () => {
-  return (
-    <div>
-          <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
-  )
+import axios from "axios";
+import VideoCard from "../components/VideoCard";
+import NoResults from "../components/NoResults";
+
+interface IProps {
+  videos: Video[];
 }
 
-export default Home
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"no Videos"} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
+};
+
+export default Home;
