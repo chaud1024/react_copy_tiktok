39dc4ba8f0c0f3268849b9b3658395884f420a9c
chaud1024 <chaud1024@gmail.com>
2022-08-31 21:37:41 +0900

    add video

--- react_copy_tiktok/next.config.js
+++ react_copy_tiktok/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["media.cdn-rouje.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
